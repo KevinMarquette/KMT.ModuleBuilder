698fff81323347dbe3273458bda7731698b35543
kevinmarquette <kevmar@gmail.com>
2019-10-04 01:24:26 -0700

    add reference module test data

---
+++ Tests/Data/SampleModule/build.ps1
@@ -0,0 +1,38 @@
+[CmdletBinding()]
+
+param($Task = 'Default')
+
+$Script:Modules = @(
+    'BuildHelpers',
+    'InvokeBuild',
+    'Pester',
+    'platyPS',
+    'PSScriptAnalyzer',
+    'DependsOn'
+)
+
+$Script:ModuleInstallScope = 'CurrentUser'
+
+'Starting build...'
+'Installing module dependencies...'
+
+Get-PackageProvider -Name 'NuGet' -ForceBootstrap | Out-Null
+
+Install-Module -Name $Script:Modules -Scope $Script:ModuleInstallScope -AllowClobber -Force -SkipPublisherCheck
+
+Set-BuildEnvironment
+Get-ChildItem Env:BH*
+Get-ChildItem Env:APPVEYOR*
+
+$Error.Clear()
+
+'Invoking build...'
+
+Invoke-Build $Task -Result 'Result'
+if ($Result.Error)
+{
+    $Error[-1].ScriptStackTrace | Out-String
+    exit 1
+}
+
+exit 0
