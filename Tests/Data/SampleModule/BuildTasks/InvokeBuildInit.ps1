73e98c6c709dad4084451791272993402f5d766b
kevinmarquette <kevmar@gmail.com>
2019-10-05 03:03:47 -0700

    Remove Global Variables

--- Tests/Data/SampleModule/BuildTasks/InvokeBuildInit.ps1
+++ Tests/Data/SampleModule/BuildTasks/InvokeBuildInit.ps1
@@ -2,33 +2,10 @@ Write-Verbose "Initializing build variables" -Verbose
 Write-Verbose "  Existing BuildRoot [$BuildRoot]" -Verbose
 
 Initialize-KTMModuleProject -ModuleName SampleModule -BuildRoot "$testdrive\SampleModule"
-#$init = Get-KTMBuildVariable
-
-$Script:DocsPath = $init.DocsPath
-Write-Verbose "  DocsPath [$DocsPath]" -Verbose
-
-$Script:Output = $init.Output
-Write-Verbose "  Output [$Output]" -Verbose
-
-$Script:Source = $init.Source
-Write-Verbose "  Source [$Source]" -Verbose
-
-$Script:Destination = $init.Destination
-Write-Verbose "  Destination [$Destination]" -Verbose
-
-$Script:ManifestPath = $init.ManifestPath
-Write-Verbose "  ManifestPath [$ManifestPath]" -Verbose
-
-$Script:ModulePath = $init.ModulePath
-Write-Verbose "  ModulePath [$ModulePath]" -Verbose
-
-$Script:Folders = $init.Folders
-Write-Verbose "  Folders [$Folders]" -Verbose
-
-$Script:TestFile = $init.TestFile
-Write-Verbose "  TestFile [$TestFile]" -Verbose
-
-$Script:PSRepository = $init.PSRepository
-Write-Verbose "  PSRepository [$TestFile]" -Verbose
+$init = Get-KTMBuildVariable
+foreach($key in $init.Keys)
+{
+    Write-Verbose "  $key [$($init[$key])]" -Verbose
+}
 
 function taskx($Name, $Parameters) { task $Name @Parameters -Source $MyInvocation }
