6737deeef6ec303049f49fbada0a0e488a1cb1f4
kevinmarquette <kevmar@gmail.com>
2019-10-06 21:02:56 -0700

    add Invoke-KmtDotNetCompileTask

---
+++ KMT.ModuleBuilder/Public/Invoke-KmtCompileTask.ps1
@@ -0,0 +1,39 @@
+function Invoke-KmtDotNetCompileTask
+{
+    <#
+        .Synopsis
+        Compiles DotNet binary module
+        .Example
+        Invoke-KmtCompileTask -Path $Path
+
+        .Notes
+
+    #>
+    [cmdletbinding()]
+    param()
+
+    end
+    {
+        try
+        {
+            $buildRoot = (Get-KmtBuildVariable).BuildRoot
+            $destination = (Get-KmtBuildVariable).Destination
+
+            $csproj = Get-ChildItem -Path $buildRoot -Include *.csproj -Recurse
+            if ($csproj)
+            {
+
+                # This build command requires .Net Core
+                # TODO add check for dotnet
+                Write-Verbose "Building Binary Module"
+                $csproj = Get-ChildItem -Path $BuildRoot -Include *.csproj -Recurse
+                $folder = Split-Path $csproj
+                dotnet build $folder -c Release -o $Destination\bin
+            }
+        }
+        catch
+        {
+            $PSCmdlet.ThrowTerminatingError( $PSItem )
+        }
+    }
+}
--- Tests/Data/SampleModule/BuildTasks/Compile.Task.ps1
+++ Tests/Data/SampleModule/BuildTasks/Compile.Task.ps1
@@ -1,17 +1,9 @@
 taskx Compile @{
-    If = (Get-ChildItem -Path (Get-KmtBuildVariable).BuildRoot -Include *.csproj -Recurse)
     Inputs = {
         Get-ChildItem (Get-KmtBuildVariable).BuildRoot -Recurse -File -Include *.cs
     }
     Outputs = "{0}\bin\{1}.dll" -f (Get-KmtBuildVariable).Destination,(Get-KmtBuildVariable).ModuleName
     Jobs = {
-        $buildRoot = (Get-KmtBuildVariable).BuildRoot
-        $destination = (Get-KmtBuildVariable).Destination
-
-        # This build command requires .Net Core
-        "Building Module"
-        $csproj = Get-ChildItem -Path $BuildRoot -Include *.csproj -Recurse
-        $folder = Split-Path $csproj
-        dotnet build $folder -c Release -o $Destination\bin
+        Invoke-KmtDotNetCompileTask
     }
 }
