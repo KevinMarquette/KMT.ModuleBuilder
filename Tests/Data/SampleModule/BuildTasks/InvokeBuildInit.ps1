61278d91aa19fb46fa4a93e4a00aa328b1164e05
kevinmarquette <kevmar@gmail.com>
2019-10-07 23:39:00 -0700

    rename Update-KmtSemVerTask

--- Tests/Data/SampleModule/BuildTasks/InvokeBuildInit.ps1
+++ Tests/Data/SampleModule/BuildTasks/InvokeBuildInit.ps1
@@ -97,20 +97,7 @@ task PublishVersion {
 
 
 task SetVersion {
-    $task = @{
-        ManifestPath = (Get-KmtBuildVariable).ManifestPath
-        ModuleName = (Get-KmtBuildVariable).ModuleName
-        Repository = (Get-KmtBuildVariable).Repository
-    }
-
-    Update-KmtSemVerTask -Verbose @task
-
-    # This cleans up files from previous implementation
-    $BuildRoot = (Get-KmtBuildVariable).BuildRoot
-    if(Test-Path $BuildRoot\fingerprint)
-    {
-        Remove-Item $BuildRoot\fingerprint
-    }
+    Invoke-KmtSemVerTask
 }
 
 
