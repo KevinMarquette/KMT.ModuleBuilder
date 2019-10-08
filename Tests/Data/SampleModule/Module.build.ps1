ef7f42ae5d1f42ef2213ce0339d284aab8dafcc4
kevinmarquette <kevmar@gmail.com>
2019-10-08 00:19:35 -0700

    remove build task files

--- Tests/Data/SampleModule/Module.build.ps1
+++ Tests/Data/SampleModule/Module.build.ps1
@@ -1,6 +1,6 @@
 $Script:ModuleName = Get-ChildItem .\*\*.psm1 | Select-object -ExpandProperty BaseName
 $Script:CodeCoveragePercent = 0.0 # 0 to 1
-. $psscriptroot\BuildTasks\InvokeBuildInit.ps1
+
 
 task Default Build, Helpify, Test, UpdateSource
 task Build Copy, Compile, BuildModule, BuildManifest, SetVersion
@@ -10,6 +10,88 @@ task Publish Build, PublishVersion, Helpify, Test, PublishModule
 task TFS Clean, Build, PublishVersion, Helpify, Test
 task DevTest ImportDevModule, Pester
 
-Write-Host 'Import common tasks'
-Get-ChildItem -Path $buildroot\BuildTasks\*.Task.ps1 |
-    ForEach-Object {Write-Host $_.FullName;. $_.FullName}
+Write-Verbose "Initializing build variables" -Verbose
+Write-Verbose "  Existing BuildRoot [$BuildRoot]" -Verbose
+
+Initialize-KmtModuleProject -ModuleName SampleModule -BuildRoot "$testdrive\SampleModule"
+$init = Get-KmtBuildVariable
+foreach($key in $init.Keys)
+{
+    Write-Verbose "  $key [$($init[$key])]" -Verbose
+}
+
+function taskx($Name, $Parameters) { task $Name @Parameters -Source $MyInvocation }
+task Analyze {
+    Invoke-KmtAnalyzeTask
+}
+
+taskx BuildManifest @{
+    Jobs    = {
+        Invoke-KmtBuildManifestTask
+    }
+}
+
+taskx BuildModule @{
+
+    Jobs    = {
+        Invoke-KmtBuildModuleTask
+    }
+}
+
+task Clean {
+    Invoke-KmtCleanTask
+}
+
+taskx Compile @{
+    Jobs = {
+        Invoke-KmtDotNetCompileTask
+    }
+}
+
+task Copy {
+    Invoke-KmtCopyTask
+}
+
+task GenerateHelp {
+    Invoke-KmtGenerateHelpTask
+}
+
+task GenerateMarkdown {
+    Invoke-KmtGenerateMarkdown
+}
+
+task ImportDevModule {
+    Invoke-KmtImportDevModuleTask
+}
+
+task ImportModule {
+    Invoke-KmtImportBuiltModuleTask
+}
+
+task Install Uninstall, {
+    Invoke-KmtInstallModule
+}
+
+task Pester {
+    Invoke-KmtPesterTask
+}
+
+task PublishModule {
+    Invoke-KmtPublishModuleTask
+}
+
+task PublishVersion {
+    Invoke-KmtPublishVersionTask
+}
+
+task SetVersion {
+    Invoke-KmtSemVerTask
+}
+
+task Uninstall {
+    Invoke-KtmUninstallModule
+}
+
+task UpdateSource {
+    Invoke-KmtUpdateSourceTask
+}
