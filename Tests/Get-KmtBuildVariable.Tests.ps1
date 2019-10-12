1991e7450ed0766d3a0b352dac3086aee04c9f96
kevinmarquette <kevmar@gmail.com>
2019-10-12 01:50:38 -0700

    linux tests

--- Tests/Get-KmtBuildVariable.Tests.ps1
+++ Tests/Get-KmtBuildVariable.Tests.ps1
@@ -1,24 +1,25 @@
+using namespace System.IO
 Describe 'Function Get-KmtBuildVariable' -tag build {
 
     BeforeAll {
-        $path = "$testdrive\SampleModule"
+        $path = "$testdrive/SampleModule"
         New-Item -Type Directory $path -ErrorAction Ignore
-        Copy-Item "$PSScriptRoot\Data\SampleModule\module.kmt.json" -Destination $path
+        Copy-Item "$PSScriptRoot/Data/SampleModule/module.kmt.json" -Destination $path
         Initialize-KmtModuleProject -Path $Path
         $data = Get-KmtBuildVariable
     }
 
     $testCases = @(
         @{name='ModuleName'; value="SampleModule"}
-        @{name='BuildRoot'; value="$testdrive\SampleModule"}
-        @{name='DocsPath'; value="$testdrive\SampleModule\Docs"}
-        @{name='Output'; value="$testdrive\SampleModule\Output"}
-        @{name='Source'; value="$testdrive\SampleModule\SampleModule"}
-        @{name='Destination'; value="$testdrive\SampleModule\Output\SampleModule"}
-        @{name='ManifestPath'; value="$testdrive\SampleModule\Output\SampleModule\SampleModule.psd1"}
-        @{name='ModulePath'; value="$testdrive\SampleModule\Output\SampleModule\SampleModule.psm1"}
+        @{name='BuildRoot'; value=[path]::Join($testdrive,"SampleModule")}
+        @{name='DocsPath'; value=[path]::Join($testdrive,'SampleModule','Docs')}
+        @{name='Output'; value=[path]::Join($testdrive,'SampleModule','Output')}
+        @{name='Source'; value=[path]::Join($testdrive,'SampleModule','SampleModule')}
+        @{name='Destination'; value=[path]::Join($testdrive,'SampleModule','Output','SampleModule')}
+        @{name='ManifestPath'; value=[path]::Join($testdrive,'SampleModule','Output','SampleModule','SampleModule.psd1')}
+        @{name='ModulePath'; value=[path]::Join($testdrive,'SampleModule','Output','SampleModule','SampleModule.psm1')}
         @{name='Folders'; value=@('Classes','Includes','Internal','Private','Public','Resources')}
-        @{name='TestFile'; value="$testdrive\SampleModule\Output\TestResults_PS_.xml"}
+        @{name='TestFile'; value=[path]::Join($testdrive,'SampleModule','Output','TestResults_PS_.xml')}
         @{name='PSRepository'; value="PSGallery"}
     )
     It 'Init data has [<name>] property' -TestCases $testCases {
