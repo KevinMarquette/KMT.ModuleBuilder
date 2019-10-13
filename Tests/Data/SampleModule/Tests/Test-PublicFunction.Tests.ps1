3381b1a279276eaedf17d78f9ffe1313ee5c6348
kevinmarquette <kevmar@gmail.com>
2019-10-12 19:03:07 -0700

    Skip test module test

--- Tests/Data/SampleModule/Tests/Test-PublicFunction.Tests.ps1
+++ Tests/Data/SampleModule/Tests/Test-PublicFunction.Tests.ps1
@@ -1,5 +1,5 @@
-Describe 'Function Test-PublicFunction' -Tag build {
-    It 'Should not throw' {
+Describe 'Function Test-PublicFunction' -Tag Build {
+    It 'Should not throw' -Skip {
         Test-PublicFunction -Path $PWD
     }
 }
